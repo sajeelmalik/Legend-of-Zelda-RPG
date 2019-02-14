644f4abcd5fddf433d4cc432d00b0b5c7827f53b
Sajeel Malik <sajeel23@berkeley.edu>
2019-02-14 11:51:54 -0800

    finalize loading graphics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,20 @@ $(document).ready(function () {
     var storms = new Audio("assets/images/songofstorms.mp3");
     var opacityVal = 0.7;
 
-    var loadingFunc = function(name){
-        if ($('#loading').css('visibility') == 'visible') {
-                var percentage = 0;
-                $("#loading-text").text(name);
-                setInterval(function () {
-                    $(".progress-bar").css("width", percentage + "");
-                    $(".progress-bar").css("aria-valuenow", percentage + "");
-                    percentage += percentage > 50 ? 20 : 10;
+    var loadingFunc = function (name) {
 
-                }, 100)
-            }
+        if ($('#loading').css('visibility') == 'visible') {
+            $(".loading-text").text(name);
+            var percentage = 0;
+            setInterval(function () {
+                percentage += percentage > 50 ? 25 : 15;
+                $(".progress-bar").css("width", percentage + "");
+                $(".progress-bar").css("aria-valuenow", percentage + "");
+            }, 100)
+        }
     }
 
+    // establish a timed loading functionality for ES5 without implementing promises or async/await
     $("#start").on("click", function () {
         storms.play();
         $("#start").css("animation-name", "force");
@@ -27,15 +28,33 @@ $(document).ready(function () {
             // $("#start").css("display", "none");
             $("#start").fadeOut("slow");
             // $("#loading").fadeIn("slow");
-            $("#loading").css("visibility", "visible");
-            $("#loading").fadeIn("slow");
 
-            // $("#loading").css("display", "flex");
+            // $("#loading").css("display", "flex");     
 
-            loadingFunc("LOADING ASSETS");
-            loadingFunc("LOADING GRAPHICS");            
-            
         }, 5000)
+
+        setTimeout(function () {
+            $("#loading").css("visibility", "visible");
+            loadingFunc("LOADING ASSETS");
+
+            $(".progress-bar").css("width", "0");
+            $(".progress-bar").css("aria-valuenow", "0");
+        }, 6000)
+
+        setTimeout(function () {
+            $(".progress-bar").removeClass("bg-success");
+            $(".progress-bar").addClass("bg-info");
+            loadingFunc("LOADING GRAPHICS");
+
+            $(".progress-bar").css("width", "0");
+            $(".progress-bar").css("aria-valuenow", "0");
+        }, 10000)
+
+        setTimeout(function () {
+            $(".progress-bar").removeClass("bg-info");
+            $(".progress-bar").addClass("bg-warning");
+            loadingFunc("LOADING CHARACTERS");
+        }, 14000)
 
     });
 
