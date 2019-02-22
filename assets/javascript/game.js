ad9b4d74bce50151b8f5d3f2a39ac59f8d499a00
Sajeel Malik <sajeel23@berkeley.edu>
2019-02-22 13:08:40 -0800

    finalize primary game logic, initialize animations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
 
-    var storms = new Audio("assets/images/songofstorms.mp3");
+    // initialize audio
+    var storms = new Audio("assets/audio/songofstorms.mp3");
+    var victory = new Audio("assets/audio/victory.mp3");
+    var chooseCharacter = new Audio("assets/audio/chooseyourcharacter.wav");
+    chooseCharacter.volume = 0.6;
+
+
+    // initialize global variables
     var KOs = 0;
     var wins = 0;
     var losses = 0;
@@ -14,8 +21,9 @@ $(document).ready(function () {
             name: "Link",
             health: 110,
             attack: 25,
-            counter: 11,
-            image: "assets/images/link.png"
+            counter: 13,
+            image: "assets/images/link.png",
+            chosenAudio: "assets/audio/link.wav"
         },
         {
             id: "#zelda",
@@ -23,15 +31,17 @@ $(document).ready(function () {
             health: 150,
             attack: 1,
             counter: 17,
-            image: "assets/images/zelda.png"
+            image: "assets/images/zelda.png",
+            chosenAudio: "assets/audio/zelda.wav"
         },
         {
             id: "#sheik",
             name: "Sheik",
             health: 120,
-            attack: 11,
+            attack: 18,
             counter: 14,
-            image: "assets/images/sheik.png"
+            image: "assets/images/sheik.png",
+            chosenAudio: "assets/audio/sheik.wav"
         },
         {
             id: "#ganon",
@@ -39,7 +49,8 @@ $(document).ready(function () {
             health: 160,
             attack: 4,
             counter: 21,
-            image: "assets/images/ganondorf.png"
+            image: "assets/images/ganondorf.png",
+            chosenAudio: "assets/audio/ganondorf.wav"
         }
     ]
 
@@ -87,6 +98,7 @@ $(document).ready(function () {
             $("#loading").fadeOut(500);
             $("#main").fadeIn(900);
             embedCharacters();
+            chooseCharacter.play();
         }, 13800)
 
     });
@@ -97,6 +109,10 @@ $(document).ready(function () {
             hasChosenCharacter = true;
             var characterData = JSON.parse($(this).attr("data"));
             hero = characterData;
+            console.log(hero);
+            var charSelectAudio = new Audio(hero.chosenAudio);
+            charSelectAudio.play();
+
             $(this).parent().hide();
 
             // sets up fighting area with chosen character
@@ -119,8 +135,12 @@ $(document).ready(function () {
                 if (hasChosenOpponent === false) {
 
                     hasChosenOpponent = true;
+                    $("#fight-button").show();
                     var characterData = JSON.parse($(this).attr("data"));
                     opponent = characterData;
+                    var oppSelectAudio = new Audio(opponent.chosenAudio);
+                    oppSelectAudio.play();
+
                     $(this).parent().hide();
 
                     // sets up fighting area with opponent
@@ -128,6 +148,7 @@ $(document).ready(function () {
                     $("#opponent-name").text(opponent.name);
                     console.log(opponent);
                     $("#opponent-health").text(opponent.health);
+                    $("#opponent-health").css("background-color", "#007bff");
 
                     // redisplay fighting functionality
                     $("#message").text("FIGHT!");
@@ -143,36 +164,41 @@ $(document).ready(function () {
 
     $(document).on("click", "#fight-button", function () {
 
-        if(hero.health > 0 && opponent.health > 0) {
+        if (hero.health > 0 && opponent.health > 0) {
             hero.health -= opponent.counter;
             opponent.health -= hero.attack;
             hero.attack += hero.counter;
 
             console.log(hero.attack)
+
+            if (hero.health <= 0 || opponent.health <= 0) {
+                $(this).hide();
+            }
         }
+
 
         // update health badge
         $("#chosen-health").text(hero.health > 0 ? hero.health : 0);
         $("#opponent-health").text(opponent.health > 0 ? opponent.health : 0);
 
         // change health badge color
-        if(hero.health <= 100 && hero.health > 60){
+        if (hero.health <= 100 && hero.health > 60) {
             $("#chosen-health").css("background-color", "#28a745");
         }
-        else if(hero.health <= 60 && hero.health > 30){
+        else if (hero.health <= 60 && hero.health > 30) {
             $("#chosen-health").css("background-color", "orange");
         }
-        else if(hero.health <= 30){
+        else if (hero.health <= 30) {
             $("#chosen-health").css("background-color", "#dc3545");
         }
-        
-        if(opponent.health <= 100 && opponent.health > 60){
+
+        if (opponent.health <= 100 && opponent.health > 60) {
             $("#opponent-health").css("background-color", "#28a745");
         }
-        else if(opponent.health <= 60 && opponent.health > 30){
+        else if (opponent.health <= 60 && opponent.health > 30) {
             $("#opponent-health").css("background-color", "orange");
         }
-        else if(opponent.health <= 30){
+        else if (opponent.health <= 30) {
             $("#opponent-health").css("background-color", "#dc3545");
         }
 
@@ -180,23 +206,65 @@ $(document).ready(function () {
             losses++;
             $("#message").text("YOU LOSE!");
 
-        } else if (opponent.health <= 0) {
+        } else if (opponent.health <= 0 && KOs < 3) {
+            // var particles = new Particles('#opponent');
+
+            // // Disintegrate the button into particles
+            // particles.disintegrate();
+
             KOs++;
             hasChosenOpponent = false;
+            hero.health += 25;
+            $("#message #add-health").text("+25 health!  ")
+            updateHealth();
 
-            $("#message").text("Choose your next challenge!");
-            $("#message").css("font-size", "1.5em");
+            if ($("#chosen-health").hasClass("run-animation")) {
+                $("#chosen-health").removeClass("run-animation");
+                setTimeout(function () {
+                    $("#chosen-health").addClass("run-animation");
+
+                }, 500)
+            }
+            else {
+                $("#chosen-health").addClass("run-animation");
+            }
+
+            $("#add-health").fadeOut(2000)
+
+            // $("#chosen-health").css("animation-name", "pulse");
+            // $("#chosen-health").css("animation-duration", "1s");
+            // $("#chosen-health").css("animation-iteration-count", "1");
+
+
 
             // check if the player has defeated all the opponents
             if (KOs === 3) {
                 wins++;
                 $("#message").text("You've defeated all the villians!");
+                $("#message").css("font-size", "2em");
+                $("#opponent-div").hide(500);
+                $("#opponent-div").removeClass("d-flex");
+                $("#chosen-stats").hide();
+                $("#opponent-div").css("display", "none !important");
+                $("#chosen-character").css("transform", "scale(1.17)");
+                // console.log($("#opponent").parent());
+                // $("#fighting-area").addClass("justify-content-center");
+
+                setTimeout(function () {
+                    victory.play();
+                    storms.volume = 0.0;
+                }, 1000)
+
+
+
+            }
+            else {
+                $("#message").text("Choose your next challenger!");
                 $("#message").css("font-size", "1.5em");
             }
         }
 
     })
-
 
 
     // sets up loading animations
@@ -224,6 +292,33 @@ $(document).ready(function () {
         for (var i = 0; i < characters.length; i++) {
             $(characters[i].id).attr("data", JSON.stringify(characters[i]));
             // console.log(characters[i]);
+        }
+    }
+
+    function updateHealth() {
+        // update health badge
+        $("#chosen-health").text(hero.health > 0 ? hero.health : 0);
+        $("#opponent-health").text(opponent.health > 0 ? opponent.health : 0);
+
+        // change health badge color
+        if (hero.health <= 100 && hero.health > 60) {
+            $("#chosen-health").css("background-color", "#28a745");
+        }
+        else if (hero.health <= 60 && hero.health > 30) {
+            $("#chosen-health").css("background-color", "orange");
+        }
+        else if (hero.health <= 30) {
+            $("#chosen-health").css("background-color", "#dc3545");
+        }
+
+        if (opponent.health <= 100 && opponent.health > 60) {
+            $("#opponent-health").css("background-color", "#28a745");
+        }
+        else if (opponent.health <= 60 && opponent.health > 30) {
+            $("#opponent-health").css("background-color", "orange");
+        }
+        else if (opponent.health <= 30) {
+            $("#opponent-health").css("background-color", "#dc3545");
         }
     }
 
