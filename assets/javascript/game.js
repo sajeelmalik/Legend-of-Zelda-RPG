d2be5a46b7f38b42bbe917360aa56cedbe35f9c6
Sajeel Malik <sajeel23@berkeley.edu>
2019-02-21 22:54:20 -0800

    large-scale game logic update, set up fighting graphics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
     var storms = new Audio("assets/images/songofstorms.mp3");
+    var KOs = 0;
     var wins = 0;
     var losses = 0;
     var hasChosenCharacter = false;
@@ -12,24 +13,24 @@ $(document).ready(function () {
             id: "#link",
             name: "Link",
             health: 110,
-            attack: 17,
-            defense: 5,
+            attack: 25,
+            counter: 11,
             image: "assets/images/link.png"
         },
         {
             id: "#zelda",
             name: "Zelda",
             health: 150,
-            attack: 5,
-            defense: 17,
+            attack: 1,
+            counter: 17,
             image: "assets/images/zelda.png"
         },
         {
             id: "#sheik",
             name: "Sheik",
             health: 120,
-            attack: 9,
-            defense: 8,
+            attack: 11,
+            counter: 14,
             image: "assets/images/sheik.png"
         },
         {
@@ -37,10 +38,13 @@ $(document).ready(function () {
             name: "Ganondorf",
             health: 160,
             attack: 4,
-            defense: 21,
+            counter: 21,
             image: "assets/images/ganondorf.png"
         }
     ]
+
+    var hero;
+    var opponent;
 
     // establish a timed loading functionality for ES5 without implementing promises or async/await
     $("#start").on("click", function () {
@@ -69,21 +73,21 @@ $(document).ready(function () {
             $(".progress-bar").removeClass("bg-success");
             $(".progress-bar").addClass("bg-info");
             loadingFunc("LOADING GRAPHICS");
-        }, 10000)
+        }, 9000)
 
         setTimeout(function () {
 
             $(".progress-bar").removeClass("bg-info");
             $(".progress-bar").addClass("bg-warning");
             loadingFunc("LOADING CHARACTERS");
-        }, 14000)
+        }, 10700)
 
         setTimeout(function () {
 
             $("#loading").fadeOut(500);
-            $("#main").fadeIn("slow");
+            $("#main").fadeIn(900);
             embedCharacters();
-        }, 18000)
+        }, 13800)
 
     });
 
@@ -92,7 +96,10 @@ $(document).ready(function () {
 
             hasChosenCharacter = true;
             var characterData = JSON.parse($(this).attr("data"));
+            hero = characterData;
+            $(this).parent().hide();
 
+            // sets up fighting area with chosen character
             $("#chosen-character").attr("src", $(this).attr("src"));
             $("#chosen-name").text(characterData.name);
             console.log(characterData);
@@ -113,12 +120,19 @@ $(document).ready(function () {
 
                     hasChosenOpponent = true;
                     var characterData = JSON.parse($(this).attr("data"));
+                    opponent = characterData;
+                    $(this).parent().hide();
 
+                    // sets up fighting area with opponent
                     $("#opponent").attr("src", $(this).attr("src"));
-                    $("#opponent-name").text(characterData.name);
-                    console.log(characterData);
-                    $("#opponent-health").text(characterData.health);
+                    $("#opponent-name").text(opponent.name);
+                    console.log(opponent);
+                    $("#opponent-health").text(opponent.health);
+
+                    // redisplay fighting functionality
                     $("#message").text("FIGHT!");
+                    $("#message").css("font-size", "2.0em");
+                    $("#message").css("font-weight", "bold");
                     $("#fight-button").show();
                 }
             })
@@ -126,6 +140,62 @@ $(document).ready(function () {
 
 
     });
+
+    $(document).on("click", "#fight-button", function () {
+
+        if(hero.health > 0 && opponent.health > 0) {
+            hero.health -= opponent.counter;
+            opponent.health -= hero.attack;
+            hero.attack += hero.counter;
+
+            console.log(hero.attack)
+        }
+
+        // update health badge
+        $("#chosen-health").text(hero.health > 0 ? hero.health : 0);
+        $("#opponent-health").text(opponent.health > 0 ? opponent.health : 0);
+
+        // change health badge color
+        if(hero.health <= 100 && hero.health > 60){
+            $("#chosen-health").css("background-color", "#28a745");
+        }
+        else if(hero.health <= 60 && hero.health > 30){
+            $("#chosen-health").css("background-color", "orange");
+        }
+        else if(hero.health <= 30){
+            $("#chosen-health").css("background-color", "#dc3545");
+        }
+        
+        if(opponent.health <= 100 && opponent.health > 60){
+            $("#opponent-health").css("background-color", "#28a745");
+        }
+        else if(opponent.health <= 60 && opponent.health > 30){
+            $("#opponent-health").css("background-color", "orange");
+        }
+        else if(opponent.health <= 30){
+            $("#opponent-health").css("background-color", "#dc3545");
+        }
+
+        if (hero.health <= 0) {
+            losses++;
+            $("#message").text("YOU LOSE!");
+
+        } else if (opponent.health <= 0) {
+            KOs++;
+            hasChosenOpponent = false;
+
+            $("#message").text("Choose your next challenge!");
+            $("#message").css("font-size", "1.5em");
+
+            // check if the player has defeated all the opponents
+            if (KOs === 3) {
+                wins++;
+                $("#message").text("You've defeated all the villians!");
+                $("#message").css("font-size", "1.5em");
+            }
+        }
+
+    })
 
 
 
